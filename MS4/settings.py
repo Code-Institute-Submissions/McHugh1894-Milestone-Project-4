ead8418c9059709f0ce5f7e15552b07d1ac7214a
McHugh1894 <mchugh1894@gmail.com>
2021-10-21 12:22:41 +0000

    Updated settings.py

--- MS4/settings.py
+++ MS4/settings.py
@@ -121,7 +121,7 @@ WSGI_APPLICATION = 'MS4.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('postgres://dzmtgzjkmrfmlm:39dc726eae2683934bc4d4'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
