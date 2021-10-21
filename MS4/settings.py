54652c9df3653c9df85e6b410739da061e788968
McHugh1894 <mchugh1894@gmail.com>
2021-10-21 12:45:41 +0000

    Updated bucket name in settings.py

--- MS4/settings.py
+++ MS4/settings.py
@@ -176,7 +176,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'McHugh1894-fit4life-oisin'
+    AWS_STORAGE_BUCKET_NAME = 'fit4life-oisin'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
