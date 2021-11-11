b38c3273ed1b4756901881d77ef8d6340d60f10f
McHugh1894 <mchugh1894@gmail.com>
2021-11-11 09:57:45 +0000

    Installed slug

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+
+# admin for blogs.
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'title',
+        'body',
+        'intro',
+    )
+
+    read_only = ('slug',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
