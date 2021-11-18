c64d1de6cb746cca5e45ae2c3b70d1d40fa17d25
McHugh1894 <mchugh1894@gmail.com>
2021-11-18 14:26:27 +0000

    Contact app edited

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    
+    message = models.TextField(max_length=1000, null=False, blank=False)
+    subject = models.TextField(max_length=1000, null=False, blank=False)
+    email = models.EmailField()
+
+
+    
+        
